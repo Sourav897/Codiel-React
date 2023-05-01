2c15b550d10fd6072f2b9fcc5ccdcd5cae69c1a5
Sourav <Sauravpandeyaz897@gmail.com>
2023-05-01 20:52:40 +0530

    First commit after the Post

---
+++ src/pages/Home.js
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types";
+
+import styles from "../styles/home.module.css";
+
+const Home = ({ posts }) => {
+  return (
+    <div className={styles.postsList}>
+      {posts.map((post) => (
+        <div className={styles.postWrapper} key={`post-${post._id}`}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
+              <img
+                src="https://banner2.cleanpng.com/20180623/iqh/kisspng-computer-icons-avatar-social-media-blog-font-aweso-avatar-icon-5b2e99c40ce333.6524068515297806760528.jpg"
+                alt="user-pic"
+              />
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
+            </div>
+            <div className={styles.postContent}>{post.conent}</div>
+
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://img.icons8.com/?size=1x&id=114072&format=png"
+                  alt="likes-icon"
+                />
+                <span>5</span>
+              </div>
+
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://img.icons8.com/?size=1x&id=11167&format=png"
+                  alt="comments-icon"
+                />
+                <span>2</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              <div className={styles.postCommentsItem}>
+                <div className={styles.postCommentHeader}>
+                  <span className={styles.postCommentAuthor}>Bill</span>
+                  <span className={styles.postCommentTime}>a minute ago</span>
+                  <span className={styles.postCommentLikes}>22</span>
+                </div>
+
+                <div className={styles.postCommentContent}>Random comment</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Home.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default Home;
